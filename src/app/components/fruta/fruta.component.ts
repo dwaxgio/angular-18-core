507e008691a2925a69641cbedd9ecdc65528af6c
edward <edward_gio@hotmail.com>
2024-05-28 15:39:22 -0500

    Example custom component fruta added

---
+++ src/app/components/fruta/fruta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruta',
+  standalone: true,
+  imports: [],
+  templateUrl: './fruta.component.html',
+  styleUrl: './fruta.component.css'
+})
+export class FrutaComponent {
+
+}
